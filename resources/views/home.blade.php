5069979ceda1dc7ba420a07af4a65216036b1021
support <support@lazarus.com.ve>
2022-08-09 14:54:48 -0400

    Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
 
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam eaque ab aut maiores placeat quis, deleniti ea, dolores autem asperiores error assumenda, quas perferendis. Porro minus dolorum et fuga!</p>
+<div class="container">
+    <div class="justify-content-center">
+        <div class="col-md-8">
+            <img src="{{ asset('img/mr-robot.jpg') }}" alt="" srcset="">
+            <div class="card-body text-justify">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <h1 class="text-center">Welcome to Patricia Project</h1>
+
+                <br>
+
+                <h3>DISCLAIMER</h3>
+                <p>This app was created for <strong class="fw-bolder text-uppercase text-decoration-underline">educational purpose</strong> only.</p>
+                <p>The use of this program is COMPLETE RESPONSIBILITY of the END-USER. Developers assume NO liability and are NOT responsible for any misuse or damage caused by this program.</p>
+                    
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
